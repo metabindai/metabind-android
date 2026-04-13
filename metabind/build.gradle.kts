6b63515ef5c3a6d0128e2dcf27210a539b2d1998
Misha <misha@velosmobile.com>
2026-04-13 13:00:38 -0500

    Update version

--- metabind/build.gradle.kts
+++ metabind/build.gradle.kts
@@ -45,7 +45,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.yapstudios"
             artifactId = "metabind-android"
-            version = "0.0.5"
+            version = "0.0.6"
 
             afterEvaluate {
                 from(components["default"])
