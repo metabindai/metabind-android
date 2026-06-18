2a4fca024253bd201617c500a05d74d7c26aca2b
Misha <misha@velosmobile.com>
2026-06-18 10:21:32 -0500

    Bump metabind-android to 0.0.15 (bindjs 0.0.17)

--- metabind/build.gradle.kts
+++ metabind/build.gradle.kts
@@ -45,7 +45,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "ai.metabind"
             artifactId = "metabind-android"
-            version = "0.0.14"
+            version = "0.0.15"
 
             afterEvaluate {
                 from(components["default"])
