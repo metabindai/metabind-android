abd2697b4f9be9e79e0072928e6888fb26061dca
Misha <misha@velosmobile.com>
2026-06-29 09:58:29 -0500

    Bump metabind-assistant-android to 0.1.13 (bindjs 0.0.20)

--- _import/metabind-ai-android/metabindassistant/build.gradle.kts
+++ _import/metabind-ai-android/metabindassistant/build.gradle.kts
@@ -47,7 +47,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "ai.metabind"
             artifactId = "metabind-assistant-android"
-            version = "0.1.12"
+            version = "0.1.13"
 
             afterEvaluate {
                 from(components["default"])
